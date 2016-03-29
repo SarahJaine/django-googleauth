16255891741fc6a0462c4ce6dcaceb05ea79a987
SarahJaine <sarahjaine.sz@gmail.com>
2016-03-29 12:30:31 -0400

    changed url views from string to list

--- googleauth/urls.py
+++ googleauth/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import *
+from . import views
 
-urlpatterns = patterns('',
-    url(r'^login/$', 'googleauth.views.login', name='googleauth_login'),
-    url(r'^callback/$', 'googleauth.views.callback', name='googleauth_callback'),
-    url(r'^logout/$', 'googleauth.views.logout', name='googleauth_logout'),
-)
+urlpatterns = [
+    url(r'^login/$', views.login, name='googleauth_login'),
+    url(r'^callback/$', views.callback, name='googleauth_callback'),
+    url(r'^logout/$', views.logout, name='googleauth_logout'),
+]
